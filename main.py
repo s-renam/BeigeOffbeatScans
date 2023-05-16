78b5d889b05d3ed109f48b382eddd0248e08fa10
rrrrrendering <17344070-rrrrrendering@users.noreply.replit.com>
2023-05-16 16:50:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def age_calculator(age_list):
+    total_age = 0
+    for age in age_list:
+        total_age += age
+    average_age = total_age / len(age_list)
+    return average_age
+student_ages = [16, 16, 17, 18, 15, 17, 18]
+age_calculator(student_ages)
+print(average_age)
