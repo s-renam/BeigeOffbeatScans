049fc04c6956bf616bff1f93b5b4fe3970ca57fb
s-renam <133796874+s-renam@users.noreply.github.com>
2023-05-16 16:58:30 +0000

    waggesdhtr

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ def age_calculator(age_list):
     average_age = total_age / len(age_list)
     return average_age
 student_ages = [16, 16, 17, 18, 15, 17, 18]
-age_calculator(student_ages)
+average_age = age_calculator(student_ages)
 print(average_age)
